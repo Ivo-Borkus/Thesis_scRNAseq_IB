7634f57c99d58b5f5f2bf1ffe7e826c0aa4f4c99
Ivo Borkus <ivoborkus1@gmail.com>
2025-07-31 13:06:45 +0200

    Cleaning up the code a bit and adding more files

---
+++ Figures/Figure 3/settings.R
@@ -0,0 +1,12 @@
+print(setting)
+
+if (setting == "Response") {
+
+} else if (setting == "Primary") {
+
+
+} else if (setting == "Sample") {
+    # selected
+} else {
+    print("setting selected is not found, using different datasets as groups")
+}
