7e5cd2bee5eb41be91baa6ab4d750d4de9515afe
Ivo Borkus <ivoborkus1@gmail.com>
2025-07-31 14:03:48 +0200

    Adding signatures seperately

---
+++ Signatures/Naive.R
@@ -0,0 +1,6 @@
+# Obtained from Zwijnenburg, Anthonie Johan et al.
+# Immunity, Volume 56, Issue 8, 1955 - 1974.e10
+Naive_signature <- c(
+    "CCR7+", "PASK+", "NELL2+", "SELL+", "LDHB+", "RCAN3+", "CPA5+", "SPINT2+", "BEX3+", "TCF7+", "BEX2+", "LEF1-AS1+", "LEF1+", "LEPROTL1+", "CAMK4+",
+    "PRF1-", "NKG7-", "EFHD2-", "ADRB2-", "GZMA-", "S1PR5-", "C12ORF75-", "CCL4-", "GZMB-", "PLEK-", "CX3CR1-", "GNLY-", "GZMH-", "CTSC-", "ZEB2-"
+)
